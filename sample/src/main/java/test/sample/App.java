d0576a91fced09f7aac7e347543dba90301b6670
saivarun <www.rajb13329@gmail.com>
2017-05-12 02:49:24 -0400

    First Project

---
+++ sample/src/main/java/test/sample/App.java
@@ -0,0 +1,16 @@
+package test.sample;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+    	
+        System.out.println( "Hello World!" );
+    }
+}
